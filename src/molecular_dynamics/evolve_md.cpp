6f6a0c9dbb556d480d8414712b9702da51918f70
Alex Christison <alex.christison@live.com>
2021-11-30 03:32:41 +0000

    populate_1d_with_column_doubles added moved_too_much WIP, from 27/11 - git ssh keys broke

--- src/molecular_dynamics/evolve_md.cpp
+++ src/molecular_dynamics/evolve_md.cpp
@@ -11,7 +11,8 @@
 //
 
 // C++ standard library headers
-
+#include <cmath>
+#include <numeric>
 // Vampire headers
 #include "molecular_dynamics.hpp"
 
@@ -19,15 +20,46 @@
 #include "internal.hpp"
 
 namespace molecular_dynamics{
+   
+      namespace internal{
 
-   //----------------------------------------------------------------------------
-   // Function to initialize molecular_dynamics module
-   //----------------------------------------------------------------------------
-   void initialize(){
-
-      return;
-
-   }
-
+         //----------------------------------------------------------------------------
+         // Function to 
+         //----------------------------------------------------------------------------
+         bool moved_too_much(skin){
+            double skin;
+            double displ;
+            double displ1=0.0;
+            double displ2=0.0;
+            std::vector <double> temp_dispalcement_list;
+            int i;
+      
+            for(i=0;i<N-1;i++){
+               temp_dispalcement_list.clear()
+               temp_dispalcement_list.resize(dispalcement_list[0].size())
+            
+            
+               displ=std::sqrt(std::inner_product(dispalcement_list.begin(), dispalcement_list.end(), dispalcement_list.begin(), 0))
+            }
+      
+            return moved_too_much;
+         }
+   
+         //----------------------------------------------------------------------------
+         // Routine to populate an 1d vector of doubles with the given column of a 2d vector of doubles (of the same size)
+         //----------------------------------------------------------------------------
+   
+         void populate_1d_with_column_doubles(std::vector<int>& one_d_vector,std::vector<std::vector<int> >& two_d_vector,int index){
+      
+            int i;
+            //copy the column of the 2d vector into the row of the 1d vector
+            for(i=0;i<two_d_vector[index].size();i++){
+               one_d_vector[i] = two_d_vector[index][i];
+            }
+      
+            return;
+         }
+      }
+   
 } // end of molecular_dynamics namespace
 
