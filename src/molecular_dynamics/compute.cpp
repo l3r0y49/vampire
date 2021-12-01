049e3319ed659510f6f0854385410d307174388b
Alex Christison <alex.christison@live.com>
2021-12-01 02:17:34 +0000

    compute_forces routine a near completion WIP

--- src/molecular_dynamics/compute.cpp
+++ src/molecular_dynamics/compute.cpp
@@ -26,10 +26,7 @@ namespace molecular_dynamics{
       //----------------------------------------------------------------------------
       void define_potental_tables(){
          //temp routine variables
-         double r_sq;
-         double rm_2;
-         double rm_6;
-         double rm_12;
+         double r_sq,rm_2,rm_6,rm_12;
          int i;
          
          for(i=0;i<table_size;i++){ //may need internal::, watch for i ob1 error
@@ -45,6 +42,73 @@ namespace molecular_dynamics{
             d_phi_tab(i+1) = 24.0*rm_2*(2.0*rm_12-rm_6)  //24(1/r^14 - 1/r^8)
          }
          return;
+      }
+      //----------------------------------------------------------------------------
+      // Function to calculate forces between atoms
+      //----------------------------------------------------------------------------
+      void compute_forces(){
+         std::vector<double> sij(dimensions),rij(dimensions);
+         std::vector<double> temp_1d_pos_i(3),temp_1d_pos_j(3);
+         double r_sqij,phi,d_phi,rk,weight;
+         int i,j,k,l,m;
+         
+         Sij.resize(dimensions);
+         Rij.resize(dimensions);
+         
+         //reset force, potental energy and virial terms
+         accelerations.resize(0,std::vector<double>(0));
+         accelerations.resize(dimensions,std::vector<double>(n));
+         energy_potental.resize(0);
+         energy_potental.resize(n);
+         virial=0.0;
+         
+         for(i=0;i<n;i++){
+            //useful part of neighbour list
+            for(l=marker_1(i);l<=marker_2(i);l++){
+               j = list(l);
+               
+               //reset 1d position vector containers values without deallocating memory
+               temp_1d_pos_1.resize(0);
+               temp_1d_pos_2.resize(0);
+               temp_1d_pos_1.resize(3);
+               temp_1d_pos_2.resize(3);
+               //extract i and j atom positions
+               populate_1d_with_column_doubles(temp_1d_pos_i,positions,i);
+               populate_1d_with_column_doubles(temp_1d_pos_j,positions,j);
+               //distance between i and j
+               sij = temp_1d_pos_i - temp_1d_pos_j;
+               
+               //apply boundary conditions where needed
+               for(m=0;m<sij;m++){
+                  if(sij(m)>0.5){
+                     sij(m)-=1.0 ;
+                  }else if(sij(m)<0.5){
+                     sij(m)+=1.0;
+                  }
+               }
+               
+               rij=box_size*sij;  //real space units
+               r_sqij=std::inner_product(rij,rij);  //square distance
+               
+               if(r_sqij < pow(rcutoff,2)){   //are particles interacting?
+                  rk = (r_sqij - r_sq_min)* inv_delat_r_sq +1.0; // "continuous index in tables"
+                  k= int(rk);    //descrete index
+                  if(k<1){
+                     k = 1;      //to protect
+                  }
+                  weight = rk - double(k);            //fractional part [0,1]
+                  phi = weight* phi_tab(k+1) + (1.0-weight)* phi_tab(k); //linear interpolation
+                  dphi = weight* d_phi_tab(k+1) + (1.0-weight)* d_phi_tab(k);
+                  energy_potental(i) += 0.5*phi;     //accumulate energy
+                  energy_potental(j) += 0.5*phi;     //shared bewteen i&j
+                  virial -= dphi*r_sqij;             //accum. virial sum r(dv/dr)
+                  acc[i] += d_phi*sij                //accum. forces (Fij = -Fji)
+                  acc[j] += d_phi*sij 
+               }
+            }
+            
+            
+         }
       }
       
    }
